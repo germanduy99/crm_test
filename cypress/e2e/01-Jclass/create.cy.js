01d2f6a10def07dcb8a9c6868ed0b2ae777b4e28
Anhtt <anhtt23997@gmail.com>
2025-09-08 00:22:15 +0700

    Update disabled warning popup

--- cypress/e2e/01-Jclass/create.cy.js
+++ cypress/e2e/01-Jclass/create.cy.js
@@ -6,15 +6,10 @@ describe('Create-Class', () => {
     cy.get('a[name="login_button"]').click({ force: true });
     cy.wait(3000);
   });
-
-  it('click Module-class rồi click Create-button', () => {
-    cy.get('i.far.fa-users-class.moduleIconBlack')
-      .click({ force: true });
+  it('Không hiển thị popup cảnh báo nếu không thay đổi gì trong form', () => {
+    cy.get('i.far.fa-users-class.moduleIconBlack').click({ force: true });
     cy.get('i.far.fa-plus').click({ force: true });
-    // kiểm tra chắc chắn đã vào form Create Class
-    cy.get('form#EditView', { timeout: 20000 }).should('be.visible');
-    cy.get('form#EditView').within(() => {
-      cy.get('input[name="name"]').should('be.visible'); // field bắt buộc trong form
-    });
+    // ✅ Kiểm tra popup không xuất hiện
+    cy.get('i.far.fa-exclamation-triangle').should('not.exist');
   });
 });
